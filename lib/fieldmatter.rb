24bda0acc87d0796385ddbbaad3ba301dad8be06
Fielding Johnston <fielding@justfielding.com>
2012-02-05 00:26:32 -0600

    final commit to fix the case insensitive names issues

---
+++ lib/fieldmatter.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# FieldMatter
+# Author: Fielding Johnston
+
+# bundler
+require "bundler/setup"
+
+# external
+require "grit"
+require "JSON"
+
+# internal 
+require File.expand_path('../fieldmatter/strangematter', __FILE__)
+require File.expand_path('../fieldmatter/darkmatter', __FILE__)
+require File.expand_path('../fieldmatter/note', __FILE__)
+require File.expand_path('../fieldmatter/tag', __FILE__)
+require File.expand_path('../fieldmatter/version', __FILE__)
+
+class FieldMatter
+
+  def initialize # eventually define common attr here and inherit to DarkMatter and StrangeMatter
+
+  end
+end
+
+
+
